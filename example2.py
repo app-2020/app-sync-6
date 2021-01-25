3bf8a1c324eae31a757d9fa533e2ad16489bab3f
Pepe García <pepe@pepegar.com>
2021-01-26 00:25:25 +0100

    create example 2

---
+++ example2.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask("Example 2")
+
+@app.route("/")
+def index():
+    return render_template(
+        "example2.html",
+        name="Pepe")
+
+app.run()
